01181561dbc3e27842a33d177337d37470eaa0d7
Victor Benjamin <victor.benjamin@gmail.com>
2017-03-08 18:24:33 -0300

    basico

--- rb-autocomplete.component.ts
+++ rb-autocomplete.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'rb-autocomplete',
   templateUrl: 'rb-autocomplete.component.html',
   styleUrls: ['rb-autocomplete.component.scss']
 })
-export class RbAutocompleteComponent implements OnInit {
+export class RbAutocompleteComponent {
 
-  constructor() { }
+  _list: any[];
 
-  ngOnInit() {
+  constructor(private ref: ElementRef) {
+  }
+
+  @Input()
+  get list(): any[] {
+    return this._list;
+  }
+
+  set list(values: any[]) {
+    this._list = values;
+    const data = values.reduce((a, i) => {
+      a[i] = null;
+      return a;
+    }, {});
+    $(this.ref.nativeElement).find('input')['autocomplete']({ data });
   }
 
 }
